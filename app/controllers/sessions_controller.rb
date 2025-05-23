fcc4f0861e29b5d3a7cd9179fb27db98dc45b98d
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2025-05-23 09:04:29 +0300

    Fix the sign in/up layouts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,19 @@
 class SessionsController < ApplicationController
-  def new
-  end
+  layout "simple"
 
   def create
     user = User.find_by(email: params[:email].downcase)
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_path, notice: 'Успешно влизане!'
+      redirect_to root_path, notice: "Успешно влизане!"
     else
-      flash.now[:alert] = 'Грешен имейл или парола'
+      flash.now[:alert] = "Грешен имейл или парола"
       render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice: 'Успешно излизане!'
+    redirect_to root_path, notice: "Успешно излизане!"
   end
 end
