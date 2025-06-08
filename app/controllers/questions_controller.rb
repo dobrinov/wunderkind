d1486d6d53bbdd5bc80e9d10487e0d41a2c1bb37
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2025-06-08 18:05:32 +0300

    Use number fields if the answer is numeric
    This will improve the UX on mobile devices.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,5 +4,12 @@ class QuestionsController < AuthenticatedController
   def show
     @assignment = Assignment.find params[:assignment_id]
     @question = @assignment.questions.find params[:id]
+
+    @numeric_answer =
+      if @question.possible_answers.any?
+        false
+      else
+        @question.answer.match?(/\A[+-]?\d+\z/)
+      end
   end
 end
