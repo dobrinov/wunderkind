a6c2d0545a6d4a64d859bbb0181e454abced591e
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2025-06-08 18:05:16 +0300

    Do not include 0

--- db/math_problems/arithmetic.rb
+++ db/math_problems/arithmetic.rb
@@ -1,12 +1,12 @@
 def seed_arithmethic_questions
-  10.times do |x|
-    10.times do |y|
+  1.upto(10) do |x|
+    1.upto(10) do |y|
       Question.create! text: "#{x} + #{y} = ?", answer: x + y
     end
   end
 
   (10..20).each do |x|
-    10.times do |y|
+    1.upto(10) do |y|
       Question.create! text: "#{x} - #{y} = ?", answer: x - y
     end
   end
