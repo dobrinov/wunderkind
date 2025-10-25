e2029387dc2a52ae9499f71c87256fcb3a2fd42f
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2025-10-25 09:07:24 +0300

    Fix drop connection rake job

--- lib/tasks/database.rb
+++ lib/tasks/database.rb
@@ -1,18 +1,20 @@
-desc "Drops all database connection"
-task drop_connections: :environment do
-  env = Rails.env
-  config = Rails.configuration.database_configuration[env]
-  raise "Could not find db config entry for environment = #{env}" unless config
+namespace :db do
+  desc "Drops all database connection"
+  task drop_connections: :environment do
+    env = Rails.env
+    config = Rails.configuration.database_configuration[env]
+    raise "Could not find db config entry for environment = #{env}" unless config
 
-  database = config["primary"]["database"]
+    database = config["primary"]["database"]
 
-  ActiveRecord::Base.connection.execute <<-SQL.squish
-    SELECT pg_terminate_backend(pg_stat_activity.pid)
-    FROM pg_stat_activity
-    WHERE pg_stat_activity.datname = '#{database}'
-      AND pid <> pg_backend_pid();
-  SQL
-  ActiveRecord::Base.remove_connection
+    ActiveRecord::Base.connection.execute <<-SQL.squish
+      SELECT pg_terminate_backend(pg_stat_activity.pid)
+      FROM pg_stat_activity
+      WHERE pg_stat_activity.datname = '#{database}'
+        AND pid <> pg_backend_pid();
+    SQL
+    ActiveRecord::Base.remove_connection
+  end
+
+  task drop: :drop_connections
 end
-
-task drop: :drop_connections
