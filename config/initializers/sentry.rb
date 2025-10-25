dca99ea64b8f06f29d85453cfc8c027dc2d0261d
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2025-10-25 08:46:30 +0300

    Enable Sentry only in production

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Sentry.init do |config|
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
   config.dsn = ENV["SENTRY_DSN"]
   config.send_default_pii = true
+  config.enabled_environments = %w[production]
 end
